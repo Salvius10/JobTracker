f5e220d3951940623f020cc313cb0c26d61c2d01
Salvius10 <melvinsalvius.i@gmail.com>
2025-07-21 20:30:59 +0530

    serializer push

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class JobApplication(models.Model):
+    STAGE_CHOICES=[
+        ("Applied","Applied"),
+        ("Interview","Interview"),
+        ("Rejected","Rejected"),
+        ("Offered","Offered")
+    ]
+    company=models.CharField(max_length=150)
+    stage=models.CharField(max_length=100,choices=STAGE_CHOICES)
+    position=models.CharField(max_length=300)
+    apply_date=models.DateField()
+    response_date=models.DateField()
+    job_url=models.URLField()
+    referral=models.BooleanField(default=False)
+    resume=models.FileField(upload_to="resume/")
+
+    def __str__(self):
+        return f"{self.company} - {self.position}"
+
